b60b4de93d6e30f1b8123479f2b3bee4e5f43633
Ozal Zarbaliyev <zarbaliyevozal@gmail.com>
2026-09-06 13:38:25 +0400

    enabled class validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+import { AppValidationPipe } from './common/validators/pipes/validation.pipe.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new AppValidationPipe());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
